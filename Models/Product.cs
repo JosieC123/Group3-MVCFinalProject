fc0d81f307ea208e4151585048f47f9d89c8e8c4
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2025-05-01 20:03:09 -0400

    Nathaniel - Update Product.cs
    Added variables to determine whether a product is out of stock and/or below its reorder level. They are both dependent on pre-existing relevant information for the product.

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,9 +8,32 @@ public class Product
     [Column(TypeName = "decimal(18,4)")]
     public decimal UnitPrice { get; set; }
     public short UnitsInStock { get; set; }
+    public bool OutOfStock = false;
     public short UnitsOnOrder { get; set; }
     public short ReorderLevel { get; set; }
+    public bool IsBelowReorderLevel = false;
     public bool Discontinued { get; set; }
     public int CategoryId { get; set; }
     public Category Category { get; set; }
+
+    public void Calculation(Product product)
+    {
+        if (UnitsInStock <= 0)
+        {
+            product.OutOfStock = true;
+        }
+        else
+        {
+            product.OutOfStock = false;
+        }
+
+        if (ReorderLevel >= UnitsOnOrder)
+        {
+            product.IsBelowReorderLevel = false;
+        }
+        else
+        {
+            product.IsBelowReorderLevel = true;
+        }
+    }
 }
